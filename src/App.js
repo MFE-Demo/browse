f6d8a33c0296b7c8cc6c7d6a2b1c8a7de08d5f85
JP-Muller <mullerjp95@gmail.com>
2020-02-12 13:07:34 -0500

    added components and routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import styled from "styled-components";
+import Loading from "./Components/Loading/Loading";
+import Filter from "./Components/Filter/Filter";
+import MovieList from "./Components/MovieList/MovieList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MainColumn = styled.div`
+  max-width: 1150px;
+  margin: 0 auto;
+`;
+
+const defaultFilters = {
+  nameFilter: ""
+};
+
+const defaultHistory = createBrowserHistory();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      loading: true,
+      error: false,
+      ...defaultFilters
+    };
+  }
+
+  componentDidMount() {
+    const host = process.env.REACT_APP_CONTENT_HOST;
+    fetch(`http://www.omdbapi.com/?apikey=81fbe921&type=movie&s=harry+potter`)
+      .then(result => result.json())
+      .then(movies => {
+        this.setState({
+          movies,
+          loading: false
+        });
+      })
+      .then(console.log(this.state.movies))
+      .catch(() => {
+        this.setState({ loading: false, error: true });
+      });
+  }
+
+  setNameFilter = value => this.setState({ nameFilter: value });
+
+  resetAllFilters = () => this.setState(defaultFilters);
+
+  render() {
+    const { movies, nameFilter, loading, error } = this.state;
+
+    if (loading) {
+      return <Loading />;
+    }
+
+    if (error) {
+      return (
+        <MainColumn>
+          Sorry, but the movie list is unavailable right now
+        </MainColumn>
+      );
+    }
+
+    return (
+      <Router history={this.props.history || defaultHistory}>
+        <MainColumn>
+          <Filter
+            name={nameFilter}
+            setNameFilter={this.setNameFilter}
+            resetAll={this.resetAllFilters}
+          />
+          <MovieList movies={movies} nameFilter={nameFilter} />
+        </MainColumn>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { CircleLoader } from "react-spinners";
+
+export default () => <CircleLoader size={200} color={"lightblue"} />;
