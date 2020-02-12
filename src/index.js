b72c614149f387622c4a794df49da289cc25ae93
JP-Muller <mullerjp95@gmail.com>
2020-02-12 16:17:40 -0500

    progress

--- src/index.js
+++ src/index.js
@@ -15,14 +15,14 @@ import "react-app-polyfill/ie11";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// import { unregister } from "./registerServiceWorker";
+import { unregister } from "./serviceWorker";
 
 window.renderBrowse = (containerId, history) => {
   ReactDOM.render(
     <App history={history} />,
     document.getElementById(containerId)
   );
-//   unregister();
+  unregister();
 };
 
 window.unmountBrowse = containerId => {
